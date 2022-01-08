f77615ad79fdb4a7f277c8339d1694d3e0875600
Joel Okoromi <okmarq@gmail.com>
2022-01-08 13:10:07 +0100

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique(null,'u_email');
+            $table->string('email')->unique('email','u_email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
